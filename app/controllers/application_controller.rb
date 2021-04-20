7177515f6dd05a1cf178ced47ce4566e299595b8
toshiki-o23 <tsk.okmt.15@gmail.com>
2021-04-20 22:19:35 +0900

    [Update] sign_up sign_in bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+
+  # ==========ここから追加する==========
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    end
+  # ==========ここまで追加する==========
+
 end
