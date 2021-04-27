ae0a8f3f9f95fc9e8c33847822bdecf498f3ba77
toshiki-o23 <tsk.okmt.15@gmail.com>
2021-04-27 14:23:25 +0900

    [Add] comment実装

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,36 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  
+  def create
+    @comment = Comment.new(comment_params)
+    @post = @comment.post
+    if @comment.save
+      respond_to :js
+    else
+      flash[:alert] = "コメントに失敗しました"
+    end
+  end
+
+  def destroy
+    @like = Like.find_by(id: params[:id])
+    @post = @like.post
+    if @like.destroy
+      respond_to :js
+    end
+  end
+
+  def destroy
+    @comment = Comment.find_by(id: params[:id])
+    @post = @comment.post
+    if @comment.destroy
+      respond_to :js
+    else
+      flash[:alert] = "コメントの削除に失敗しました"
+    end
+  end
+
+  private
+    def comment_params
+      params.required(:comment).permit(:user_id, :post_id, :comment)
+    end
+end
