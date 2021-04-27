558ea48063eb2d7d082625e1a6e5a70e84514e9a
toshiki-o23 <tsk.okmt.15@gmail.com>
2021-04-27 15:09:15 +0900

    [Add] username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
 
-  # ==========ここから追加する==========
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
@@ -8,9 +7,8 @@ class ApplicationController < ActionController::Base
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :user_name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :user_name])
     end
-  # ==========ここまで追加する==========
 
 end
